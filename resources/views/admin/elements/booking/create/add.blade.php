f41674d7cc38732be46ea6ff00904ea55adfa8b6
Chi Lam <48612188+ChiLam0103@users.noreply.github.com>
2021-03-10 16:11:02 +0700

    update

--- resources/views/admin/elements/booking/create/add.blade.php
+++ resources/views/admin/elements/booking/create/add.blade.php
@@ -10,7 +10,7 @@
 
 @section('content')
     <div class="row">
-        <div class="col-lg-8">
+        <div class="col-lg-10">
             <div class="portlet light bordered">
                 <div class="portlet-title">
                     <div class="caption font-red-sunglo">
